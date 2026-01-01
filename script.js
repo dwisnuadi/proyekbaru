fa30060230908d716aa7632d72f3451ee1b44728
dwisnuadi <dwisnua71@gmail.com>
2026-01-01 21:26:00 +0800

    proyekbaru/idenx.css

---
+++ script.js
@@ -0,0 +1,89 @@
+const todoList = document.getElementById("todoList");
+const doneList = document.getElementById("doneList");
+const dateEl = document.getElementById("date");
+
+// TIME
+function updateTime() {
+  const now = new Date();
+  dateEl.textContent = now.toLocaleDateString("id-ID", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  });
+}
+updateTime();
+
+// ADD TASK
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const priority = document.getElementById("priority");
+
+  if (taskInput.value === "") {
+    alert("Tugas tidak boleh kosong!");
+    return;
+  }
+
+  const row = document.createElement("tr");
+
+  // Checkbox
+  const checkTd = document.createElement("td");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.onclick = () => markDone(row, taskInput.value);
+  checkTd.appendChild(checkbox);
+
+  // Task text
+  const taskTd = document.createElement("td");
+  taskTd.textContent = taskInput.value;
+
+  // Priority
+  const priorityTd = document.createElement("td");
+  priorityTd.textContent = priority.value;
+
+  // Delete button
+  const actionTd = document.createElement("td");
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+  delBtn.onclick = () => row.remove();
+  actionTd.appendChild(delBtn);
+
+  row.append(checkTd, taskTd, priorityTd, actionTd);
+  todoList.appendChild(row);
+
+  taskInput.value = "";
+}
+
+// DONE
+function markDone(row) {
+    const taskText = row.children[1].textContent;
+    const priority = row.children[2].textContent;
+    const doneTime = getDateTimeNow();
+  row.remove();
+  const li = document.createElement("li");
+  li.textContent = `${taskText} (${priority})`;
+  li.innerHTML = `
+    <strong>${taskText} (${priority})</strong><br>
+    <small>📅 ${doneTime}</small>
+  `;
+  doneList.appendChild(li);
+}
+
+// DELETE ALL
+function deleteAll() {
+  todoList.innerHTML = "";
+  doneList.innerHTML = "";
+}
+
+// DONE TIME 
+
+function getDateTimeNow() {
+  const now = new Date();
+  return now.toLocaleString("id-ID", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+}
